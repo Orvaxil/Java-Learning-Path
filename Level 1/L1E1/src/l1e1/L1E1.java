6206f3dd9d98d32ea268bd262239d02539bf41c3
Orvaxil <ammar.smr@hotmail.com>
2025-12-08 19:31:52 +0300

    Add levels and exercises

---
+++ Level 1/L1E1/src/l1e1/L1E1.java
@@ -0,0 +1,16 @@
+
+package l1e1;
+
+
+public class L1E1 {
+
+   
+    public static void main(String[] args) {
+     
+        System.out.println("Hello World ");
+        System.out.println("My Name is: ");
+        System.out.println("Orvaxil");
+        
+    }
+    
+}
